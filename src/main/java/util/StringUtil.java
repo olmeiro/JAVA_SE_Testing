5096cc22ad93d336d702c6fa692149b5ecce73f3
olmeiro <olme.orozco@gmail.com>
2023-04-04 22:37:06 -0500

    organizacion test JUnit

--- src/main/java/util/StringUtil.java
+++ src/main/java/util/StringUtil.java
@@ -2,6 +2,8 @@ package util;
 
 public class StringUtil {
     public static String repeat(String str, int times){
+        if(times < 0) throw new
+                IllegalArgumentException("Argument times negative");
         String result = "";
 
         for (int i = 0; i < times; i++) {
--- src/test/java/util/StringUtilTest.java
+++ src/test/java/util/StringUtilTest.java
@@ -1,13 +1,38 @@
 package util;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringUtilTest {
     @Test
-    public void testRepeat() {
-        assertEquals("holahola", StringUtil.repeat("hola",3));
-        assertEquals(StringUtil.repeat("hola",1), "hola");
+    public void repeat_string_once() {
+        assertEquals("hola", StringUtil.repeat("hola",1));
+    }
+
+    @Test
+    public void repeat_string_multiple_times() {
+        assertEquals("holaholahola", StringUtil.repeat("hola",3));
+    }
+    @Test
+    public void repeat_string_zero_times() {
+        assertEquals("", StringUtil.repeat("hola",0));
+    }
+    @Test
+    public void repeat_negative_times1() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            StringUtil.repeat("hola", -1);
+        });
+    }
+
+    @Test()
+    public void repeat_negative_times2(){
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            StringUtil.repeat("Hola", -1);
+        });
+
+        Assertions.assertEquals("Argument times negative", exception.getMessage());
     }
 }
